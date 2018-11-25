4342176e393a11f9582dd2fe32cd344cfa6f8924
Heeseock Kang <dktkwhdk1@gmail.com>
2018-11-25 00:14:55 -0800

    GUI frame_2

--- mainSubway/main.cpp
+++ mainSubway/main.cpp
@@ -1,11 +1,33 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
+#include <QString>
+#include <QDebug>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Subway");
     w.show();
 
+    /*
+    QString mFilename = "/home/dktkwhdk12/Subway_project/subway.txt";
+
+    QFile mFile(mFilename);
+
+    if(!mFile.open(QFile::ReadOnly | QFile::Text))
+    {
+        qDebug() << "could not open file for reading";
+        return 0;
+    }
+    QTextStream in(&mFile);
+    QString mText = in.readAll();
+
+    qDebug() << mText;
+
+    mFile.close();
+    */
     return a.exec();
 }
--- mainSubway/mainwindow.cpp
+++ mainSubway/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,21 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filename = "/home/dktkwhdk12/Subway_project/subway.txt";
+
+    QFile subway(filename);
+
+    if(!subway.open(QFile::ReadOnly | QFile::Text))
+    {
+        QMessageBox::information(0,"info",subway.errorString());
+    }
+    QTextStream in(&subway);
+
+    ui->textBrowser->setText(in.readAll());
+
+    subway.close();
 }
